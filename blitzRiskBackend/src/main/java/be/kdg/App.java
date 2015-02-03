1d8b53cecd2141fd29d34631a4de0116480f41f1
Jorandeboever <jorandeboever@gmail.com>
2015-02-03 10:26:37 +0100

    no message

--- blitzRiskBackend/src/main/java/be/kdg/App.java
+++ blitzRiskBackend/src/main/java/be/kdg/App.java
@@ -1,5 +1,14 @@
 package be.kdg;
 
+import be.kdg.model.User;
+import be.kdg.persistence.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Collection;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        User user = new User("joran", "jorandeboever@gmail.com", "joran");
+        session.save(user);
     }
 }
