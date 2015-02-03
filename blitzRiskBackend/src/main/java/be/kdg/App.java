0b36ae19d564c7846db97a1b5a737bb57a0b9a33
Marlies Van Hecke <marliesvanhecke@gmail.com>
2015-02-03 10:19:01 +0100

    BR22 - inloggen

--- blitzRiskBackend/src/main/java/be/kdg/App.java
+++ blitzRiskBackend/src/main/java/be/kdg/App.java
@@ -1,13 +1,23 @@
 package be.kdg;
 
+import be.kdg.model.User;
+import be.kdg.persistence.HibernateUtil;
+import org.hibernate.Session;
+
+import org.hibernate.Transaction;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        User user = new User("joran", "jorandeboever@gmail.com", "joran");
+        session.save(user);
+
     }
+
 }
---
+++ blitzRiskBackend/src/main/java/be/kdg/security/LoginService.java
@@ -0,0 +1,29 @@
+package be.kdg.security;
+
+import be.kdg.model.User;
+import be.kdg.model.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+/**
+ * Created by Marlies on 6/01/2015.
+ */
+public class LoginService implements AuthenticationProvider {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        User user = userService.checkLogin(authentication.getName(), authentication.getCredentials().toString());
+        if(user==null) return null;
+        return new UsernamePasswordAuthenticationToken(user, authentication.getCredentials(), null);
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return aClass.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
