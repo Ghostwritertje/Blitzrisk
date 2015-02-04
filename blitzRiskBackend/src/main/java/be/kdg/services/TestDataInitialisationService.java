c0a5d0660d565b5ea63dc2ae8c511a0bd8136c4e
Marlies Van Hecke <marliesvanhecke@gmail.com>
2015-02-04 14:32:19 +0100

    BR-94 opzetten security

--- blitzRiskBackend/src/main/java/be/kdg/services/TestDataInitialisationService.java
+++ blitzRiskBackend/src/main/java/be/kdg/services/TestDataInitialisationService.java
@@ -23,11 +23,11 @@ public class TestDataInitialisationService  implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         try {
             userService.addUser("Joran", "jorandeboever@gmail.com", "joran");
-       userService.addUser("Marlies", "marlies@gmail.com", "marlies");
-        userService.addUser("Alexander", "alexander@gmail.com", "alexander");
-        userService.addUser("Valter", "valter@gmail.com", "valter");
-        userService.addUser("Sven", "sven@gmail.com", "sven");
-        userService.addUser("Gunther", "gunther@gmail.com", "gunther");
+            userService.addUser("Marlies", "marlies@gmail.com", "marlies");
+            userService.addUser("Alexander", "alexander@gmail.com", "alexander");
+            userService.addUser("Valter", "valter@gmail.com", "valter");
+            userService.addUser("Sven", "sven@gmail.com", "sven");
+            userService.addUser("Gunther", "gunther@gmail.com", "gunther");
         }catch (Exception e){
             //nothing
         }
