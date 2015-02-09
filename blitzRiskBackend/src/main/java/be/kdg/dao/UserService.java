099e7fe87d5812dfc1ebdf92ab732b9ec05031dc
Marlies Van Hecke <marliesvanhecke@gmail.com>
2015-02-09 10:39:09 +0100

    versie 2 voor transactional annotations

--- blitzRiskBackend/src/main/java/be/kdg/dao/UserService.java
+++ blitzRiskBackend/src/main/java/be/kdg/dao/UserService.java
@@ -1,10 +1,8 @@
 package be.kdg.dao;
 
 import be.kdg.model.User;
-import be.kdg.persistence.HibernateUtil;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,45 +12,38 @@ import java.util.List;
  * Created by Marlies on 4/01/2015.
  */
 
+
 @Service("userService")
 public class UserService {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
     public User checkLogin(String username, String password) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Query query = session.createQuery("from User u where u.name = :username and u.password = :password");
+        Query query = sessionFactory.getCurrentSession().createQuery("from User u where u.name = :username and u.password = :password");
         query.setParameter("username", username);
         query.setParameter("password", password);
         User user = (User) query.uniqueResult();
-        session.close();
         return user;
     }
 
     public void addUser(String username, String password, String email) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        Transaction tx = session.beginTransaction();
         User user = new User();
         user.setName(username);
         user.setEmail(email);
         user.setPassword(password);
-        session.save(user);
-        tx.commit();
+        sessionFactory.getCurrentSession().save(user);
     }
 
     public User getUser(String username) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        Transaction tx = session.beginTransaction();
-        Query query = session.createQuery("from User user where user.name = :username");
+        Query query = sessionFactory.getCurrentSession().createQuery("from User where username = :username");
         query.setParameter("username", username);
-        User user = (User) query.uniqueResult();
-        tx.commit();
-        return user;
+        return (User) query.uniqueResult();
     }
 
     public List<User> findall() {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Query query = session.createQuery("from User");
-        List<User> users = query.list();
-        session.close();
-        return users;
+        Criteria criteria = sessionFactory.getCurrentSession().createCriteria(User.class);
+        return criteria.list();
     }
 
 }
