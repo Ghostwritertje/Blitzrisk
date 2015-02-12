d8af7fdc88e74e838c9f149e294f29ab56385469
Marlies Van Hecke <marliesvanhecke@gmail.com>
2015-02-12 21:02:28 +0100

    broken hibernate version

--- blitzRiskBackend/src/main/java/be/kdg/services/TestDataInitialisationService.java
+++ blitzRiskBackend/src/main/java/be/kdg/services/TestDataInitialisationService.java
@@ -19,12 +19,12 @@ public class TestDataInitialisationService  implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         try {
-      //      userService.addUser("Joran", "jorandeboever@gmail.com", "joran");
-        //    userService.addUser("Marlies", "marlies@gmail.com", "marlies");
-         //   userService.addUser("Alexander", "alexander@gmail.com", "alexander");
-         //   userService.addUser("Valter", "valter@gmail.com", "valter");
-         //   userService.addUser("Sven", "sven@gmail.com", "sven");
-         //   userService.addUser("Gunther", "gunther@gmail.com", "gunther");
+            userService.addUser("Joran", "jorandeboever@gmail.com", "joran");
+            userService.addUser("Marlies", "marlies@gmail.com", "marlies");
+            userService.addUser("Alexander", "alexander@gmail.com", "alexander");
+            userService.addUser("Valter", "valter@gmail.com", "valter");
+            userService.addUser("Sven", "sven@gmail.com", "sven");
+            userService.addUser("Gunther", "gunther@gmail.com", "gunther");
         }catch (Exception e){
             //nothing
         }
