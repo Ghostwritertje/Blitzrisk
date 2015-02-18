5e9d96348e8144451c61aae147f913f3ef86a446
Gunther <gunther.claessens@student.kdg.be>
2015-02-18 16:06:06 +0100

    interactieve map. needs services for game implementation. but first we merge!

--- blitzRiskBackend/src/main/webapp/app/components/game/gameController.js
+++ blitzRiskBackend/src/main/webapp/app/components/game/gameController.js
@@ -1,62 +1,74 @@
 'use strict';
 angular.module('blitzriskControllers').controller('GameController', ['$scope',
     function ($scope) {
-     //   alert('id');
-        /*$scope.clickRegion = function(){
-            alert('id');
-        }*/
+
+        $scope.testClass = function(){
+            alert("test");
+            var land = angular.element(this.getSVGDocument().getElementById("path5004"));
+            land.removeClass("neutralcolor");
+            land.addClass("player1color");
+
+        };
+        $scope.myData = {};
+        $scope.myData.doClick = function() {
+            alert("clicked");
+        };
     }
 ]).directive('riskmap', [function () {
     return {
         restrict: 'E',
         replace: true,
-        template: "<object type='image/svg+xml' data='assets/img/herwerkteRiskMapMini.svg'></object>",
+        template: "<object type='image/svg+xml' data='assets/img/riskMap.svg'></object>",
         link: function(scope, element, attrs) {
-            var init = function() {
-                var statusElm = angular.element(element[0]
-                    .getSVGDocument().getElementById("status"));
-                statusElm.on("click", function(event) {
-                    scope.$apply(function() {
-                        scope.currentStatus='alert';
-                    });
-                });
+            scope.init = function() {
+                var land = angular.element(element[0].getSVGDocument().getElementById("path5004"));
+                land.removeClass("neutralcolor");
+                land.addClass("player1color");
             };
-            if (element[0].getSVGDocument()) {
-                init();
-            } else {
-                element.on("load", init);
+
+            scope.changeTerritoryStyle = function(player, territory){
+                alert(territory);
+                var land = angular.element(element[0].getSVGDocument().getElementById(territory));
+                //land.removeClass("neutralcolor");
+                land.addClass("player".concat(player).concat("color"));
+                scope.hideArrows();
+                var homeTerr = angular.element(element[0].getSVGDocument().getElementById(19));
+                var homeX = homeTerr.attr("xcoord");
+                var homeY = homeTerr.attr("ycoord");
+                var homet = angular.element(element[0].getSVGDocument().getElementById(21));
+                var nX = homet.attr("xcoord");
+                var nY = homet.attr("ycoord");
+                var arrowline = "M ".concat(homeX).concat(",").concat(homeY).concat(" ").concat(nX).concat(",").concat(nY);
+                var arrow = angular.element(element[0].getSVGDocument().getElementById("arrow2"));
+                arrow.attr("d", arrowline);
+            };
+
+            scope.mouseOver = function(territory){
+                var land = angular.element(element[0].getSVGDocument().getElementById(territory));
+                land.addClass("player1colorhover");
+                /*var test = angular.element(element[0].getSVGDocument().getElementById(territory));
+                alert(test.attr("xcoord"));*/
+                //player1colorhover
+            };
+
+            scope.mouseOut = function(territory){
+                var land = angular.element(element[0].getSVGDocument().getElementById(territory));
+                //land.removeClass("player1colorhover");
+                land.attr("class", "player1color");
+            }
+
+            scope.hideArrows = function () {
+                var i;
+                for (i = 1; i < 7; ++i) {
+                    var arrow = angular.element(element[0].getSVGDocument().getElementById("arrow".concat(i)));
+                    arrow.attr("class", "arrowhidden");
+                }
+            }
+
+            scope.voidClick = function(){
+                scope.hideArrows();
+                alert("test");
             }
         }
     }
 }]);
-
-/*.directive('risk-map', [function () {
- return {
- restrict: 'A',
- scope: true,
- template: "<object id='riskMap' type='image/svg+xml' data='assets/img/status.svg'></object>",
- link: function (scope, element, attrs) {
- scope.elementId = element.attr("id");
- scope.regionClick = function () {
- alert(scope.elementId);
- };
- }
- }
- }]);
-
-
- return {
- restrict: 'AE',
- replace: true,
- template: "<object type='image/svg+xml' data='assets/img/status.svg'></object>",
- link: function (scope, element, attrs) {
- var statusChanged = function(newValue,oldValue) {
- var statusElm = angular.element(element[0].getSVGDocument().getElementById("status"));
-
- statusElm.removeClass(newValue);
- statusElm.addClass(oldValue);
- };
- scope.$watch(attrs.watch, statusChanged);
- }
- }
- */
