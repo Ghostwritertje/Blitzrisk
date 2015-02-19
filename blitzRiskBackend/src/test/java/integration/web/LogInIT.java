3218ed4e7ea4a663ae664101ee20a853fdc6406a
Marlies Van Hecke <marliesvanhecke@gmail.com>
2015-02-19 13:14:25 +0100

    all tests working and users are removed after testing

--- blitzRiskBackend/src/test/java/integration/web/LogInIT.java
+++ blitzRiskBackend/src/test/java/integration/web/LogInIT.java
@@ -2,8 +2,10 @@ package integration.web;
 
 import be.kdg.dao.UserService;
 import be.kdg.model.User;
+import be.kdg.services.UserManagerService;
 import integration.MyServerConfiguration;
 import org.junit.*;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +13,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Random;
 
@@ -19,21 +25,22 @@ import java.util.Random;
  * Created by user jorandeboever
  * Date:9/02/15.
  */
+
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/dispatcher.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
 public class LogInIT {
     private final String URL = MyServerConfiguration.URL;
     private static WebDriver driver;
+
+    @Autowired
+    private UserManagerService userService;
+
 
     @BeforeClass
     public static void insertUser() {
         System.setProperty("webdriver.chrome.driver", MyServerConfiguration.CHROMEDRIVERLOCATION);
         String workingDir = System.getProperty("user.dir");
         System.out.println(workingDir);
-        UserService userService = new UserService();
-        try{
-            userService.addUser("seleniumuser", "seleniumpass", "user@selenium.com");
-        } catch (Exception e) {
-
-        }
         driver = new ChromeDriver();
     }
 
@@ -42,21 +49,22 @@ public class LogInIT {
         driver = new ChromeDriver();
         driver.get(URL + "#/game");
         (new WebDriverWait(driver, 5)).until((WebDriver d) -> d.getCurrentUrl().equals(URL + "#/login"));
-        //(new WebDriverWait(driver, 10)).until((WebDriver d) -> !(d.getCurrentUrl().equals(URL + "#/game")));
     }
 
-    /*
+
     @Test
     public void testCorrectLogin() {
+        userService.addUser("seleniumTestUser", "seleniumTestUser", "seleniumTestUser");
         driver = new ChromeDriver();
         driver.get(URL);
         WebElement usernameElement = driver.findElement(By.id("username"));
-        usernameElement.sendKeys("Selenium3358");
+        usernameElement.sendKeys("seleniumTestUser");
         WebElement passwordElement = driver.findElement(By.id("password"));
-        passwordElement.sendKeys("Selenium3358");
+        passwordElement.sendKeys("seleniumTestUser");
         usernameElement.sendKeys(Keys.ENTER);
         (new WebDriverWait(driver, 10)).until((WebDriver d) -> d.getCurrentUrl().equals(URL + "#/game"));
-    }*/
+        userService.removeUser("seleniumTestUser");
+    }
 
     @Test
     public void testIncorrectLogin() {
@@ -105,8 +113,7 @@ public class LogInIT {
         element.sendKeys(user.getEmail());
         element.sendKeys(Keys.ENTER);
         (new WebDriverWait(driver, 5)).until((WebDriver d) -> d.findElement(By.className("errorMessage")));
-
-
+        userService.removeUser(user.getName());
     }
 
     @AfterClass
