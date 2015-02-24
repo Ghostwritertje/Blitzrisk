c879e7d8671156e380fc87d34ec602ab84dd8d4f
Joran De Boever <Jorandeboever@gmail.com>
2015-02-24 19:42:41 +0100

    Port in test klasses verandert nu automatisch naar juiste poort wanneer er wordt gedeployed naar Jetty

--- blitzRiskBackend/src/test/java/integration/MyServerConfiguration.java
+++ blitzRiskBackend/src/test/java/integration/MyServerConfiguration.java
@@ -1,19 +1,45 @@
 package integration;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by user jorandeboever
  * Date:14/02/15.
  */
 public class MyServerConfiguration {
-    //Jetty server-url: http://localhost:9090/blitzRiskBackend/
-    //Tomcat server-url: http://localhost:8080/BlitzRisk/
-    public static final String URL = "http://localhost:9090/blitzRiskBackend/";
-    private static final String CHROMEDRIVERLOCATION = System.getProperty("user.dir") +"/src/test/resources/chromedriver.exe"; //location for chromeDriver
+
+
+
+    private static final String CHROMEDRIVERLOCATION = System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe"; //location for chromeDriver
 
     public static String getChromedriverlocation() {
-        if(System.getProperty("os.name").equals("Mac OS X")) return System.getProperty("user.dir") +"/src/test/resources/chromedriver";
+        if (System.getProperty("os.name").equals("Mac OS X"))
+            return System.getProperty("user.dir") + "/src/test/resources/chromedriver";
+
         return CHROMEDRIVERLOCATION;
     }
+
+    public static String getURL() {
+        /*
+        * port.xml changes automatically when deploying to Jetty to 9090
+        * */
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        String port = "8080";
+        try {
+            port = IOUtils.toString(classloader.getResourceAsStream("port.txt"), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "http://localhost:" + port + "/BlitzRisk/";
+    }
+
+
 
 
 }
