183ce1c08ebc9bb4faf032dd19e82044a41b717f
Joran De Boever <Jorandeboever@gmail.com>
2015-02-24 12:33:33 +0100

    Coverage voor integratietesten!

--- blitzRiskBackend/src/test/java/integration/MyServerConfiguration.java
+++ blitzRiskBackend/src/test/java/integration/MyServerConfiguration.java
@@ -5,7 +5,7 @@ package integration;
  * Date:14/02/15.
  */
 public class MyServerConfiguration {
-    public static final String URL = "http://localhost:9999/BlitzRisk/";
+    public static final String URL = "http://localhost:9090/blitzRiskBackend/";
     private static final String CHROMEDRIVERLOCATION = System.getProperty("user.dir") +"/src/test/resources/chromedriver.exe"; //location for chromeDriver
 
     public static String getChromedriverlocation() {
